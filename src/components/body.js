5ce63430c619b4d015e9f85531c263fcdd097cfe
Steve Doty <stevedoty16@gmail.com>
2020-06-16 11:09:13 -0700

    basic navbar layout

--- src/components/body.js
+++ src/components/body.js
@@ -5,28 +5,7 @@ import logo from '../logo.svg'
 const Body = () => {
     return (
         <div className="body">
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                Click me!
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>Hello! I'm the body</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                Click me!
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="1">
-              <Card.Body>Hello! I'm another body</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+
             <Accordion defaultActiveKey="0">
                     <Card className="card">
                         <Card.Header>
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import {Card, ListGroup} from 'react-bootstrap'
+import {Nav, Card, ListGroup} from 'react-bootstrap'
 import logo from '../logo.svg'
 
 const Header = () => {
     return (
         <div className="header">
             <div className="navbar">
+                <Nav>
+                    <Nav.Item>
+                        HOME
+                    </Nav.Item>
+                    <Nav.Item>
+                        SEARCH
+                    </Nav.Item>
+                    <Nav.Item>
+                        DENTAL
+                    </Nav.Item>
+                    <Nav.Item>
+                        BATH
+                    </Nav.Item>
+                    <Nav.Item>
+                        BATH
+                    </Nav.Item>
+                    <Nav.Item>
+                        BATH
+                    </Nav.Item>
+                </Nav>
             </div>
             <div className="landing-page">
                 <img className="landing-page--image" src={logo}/>
